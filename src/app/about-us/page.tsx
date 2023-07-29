22fd040e1bc4dd72b3fb091d57dd9c770008e062
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2023-07-29 23:41:25 +0530

    team files from previous project

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Team from "@/app/about-us/team/Team";
+import TeamPage from "@/app/about-us/team/TeamPage";
 
 export default function Page(){
     return (
         <>
             <h2>About us</h2>
+            <TeamPage/>
         </>
     );
 }
