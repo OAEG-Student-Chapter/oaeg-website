a18dd887fd878da65f49d3fd1049664b180be666
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2023-07-30 19:18:30 +0530

    added slider to app-hero

--- src/app/components/app-hero/app-hero.tsx
+++ src/app/components/app-hero/app-hero.tsx
@@ -2,6 +2,8 @@
 import styles from "./app-hero.module.css";
 import {usePathname} from "next/navigation";
 import {routesMap} from "@/lib/routes";
+import 'react-slideshow-image/dist/styles.css'
+import {Slide} from "react-slideshow-image";
 
 interface AppHeroProps {
     height?: number | string;
@@ -10,21 +12,48 @@ interface AppHeroProps {
 export default function AppHero(props:AppHeroProps) {
 
     const pathname = usePathname()
-    let height = undefined;
+    let height : number|string|undefined = undefined;
     if (pathname !== routesMap.home.path) {
         height = '60vh';
     }
 
+    const properties = {
+        duration: 5000,
+        autoplay: true,
+        transitionDuration: 1000,
+        arrows: false,
+        infinite: true,
+        easing: "ease",
+    };
+
     return (
-        <div className={styles.heroImageContainer}
-             style={{
-                 height: height,
-                 position: "relative",
-             }}
-        >
-            {props.children}
-            <img src="/images/main-bg.png" className={styles.heroImage} alt=""/>
-        </div>
+        <Slide {...properties} canSwipe={true}>
+            {slideImages.map((slideImage, index)=> (
+                <div key={index} style={{
+                    height: height,
+                    backgroundImage: `url(${slideImage.url})`
+                }} className={styles.heroImageContainer}>
+                    <span style={spanStyle}>{slideImage.caption}</span>
+                </div>
+            ))}
+        </Slide>
+
     );
 }
 
+const spanStyle = {
+    padding: '20px',
+    background: '#efefef',
+    color: '#000000'
+}
+
+const slideImages = [
+    {
+        url: '/images/main-bg.png',
+        caption: "Caption 1"
+    },
+    {
+        url: '/images/exco.jpg',
+        caption: "Caption 2"
+    }
+]
