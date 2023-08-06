61eacfff02558941ebb7b7ad9ef8d87be7ab72e5
Sharada Shehan <sharadashehan6@gmail.com>
2023-08-06 13:29:33 +0530

    team page created

---
+++ src/app/team/Team.tsx
@@ -0,0 +1,61 @@
+import TeamMember from "../components/TeamMember";
+import React from "react";
+import Styles from "./Team.module.css";
+
+interface memberDetailObject {
+    name: string;
+    role: string;
+    twitterAccountName: string;
+    facebookAccountName: string;
+    linkedinAccountName: string;
+    avatarSRC: string;
+}
+
+interface TeamProps {
+    memberDetailList: memberDetailObject[];
+    numberOfColumns: number;
+}
+
+export default async function Team({ memberDetailList, numberOfColumns }: TeamProps) {
+    let numberOfMembers = memberDetailList.length;
+    let numberOfRows = Math.ceil(numberOfMembers / numberOfColumns);
+
+    const components = [];
+
+    for (let row = 0; row < numberOfRows; row++) {
+        const rowComponents = [];
+
+        for (let column = 0; column < numberOfColumns; column++) {
+            const index = row * numberOfColumns + column;
+            if (index < numberOfMembers) {
+                const memberDetails = memberDetailList[index];
+                const accountNames = [memberDetails.facebookAccountName, memberDetails.twitterAccountName, memberDetails.linkedinAccountName];
+
+                rowComponents.push(
+                    <div key={index} className={Styles.wrapper}>
+                        <TeamMember
+                            name={memberDetails.name}
+                            role={memberDetails.role}
+                            accountNames={accountNames}
+                            avatarSRC={memberDetails.avatarSRC}
+                        />
+                    </div>
+                );
+            }
+        }
+
+        components.push(
+            <div key={row} className={Styles.rowWrapper}>
+                {rowComponents}
+            </div>
+            );
+    }
+    
+    return (
+            <div className={Styles.teamWrapper}>
+                {components}
+            </div>
+    
+    );
+}
+
--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,7 +1,46 @@
-import TeamPage from "@/app/team/TeamPage";
+import Team from "./Team";
+import memberDetailList from "./membersDetailList.json";
+import Styles from "./Team.module.css";
+import TeamHeader from "./TeamHeader";
 
-export default function Page(){
-    return (
-        <></>
-    )
+
+interface MemberObject {
+  name: string; 
+  role: string; 
+  twitterAccountName: string; 
+  facebookAccountName: string; 
+  linkedinAccountName: string; 
+  avatarSRC: string
 }
+
+interface DataObject {
+  [key: string]: {
+    [key: string]: never[] | MemberObject[];
+  };
+}
+
+export default function Page({ params, searchParams}: {
+    params: { slug: string };
+    searchParams?: { [key: string]: string };
+  }) {
+    // By default, the year is 2023 and the body is mainBody ( hardcoded here )
+    const year:string = searchParams?.year || "2023";
+    const body:string = searchParams?.body || "mainBody";
+    const detailList: DataObject = memberDetailList;
+    return (
+        <div className={Styles.mainWrapper}>
+            <div>
+                <TeamHeader currentYear={year} currentBody={body} />
+            </div>
+            <Team
+                memberDetailList={ (year!=undefined && 
+                                    body!=undefined && 
+                                    detailList[year]!=undefined && 
+                                    detailList[year][body]!=undefined)  ?  detailList[year][body] : []
+                                }
+                numberOfColumns={5} />
+        </div>
+    );
+}
+
+
