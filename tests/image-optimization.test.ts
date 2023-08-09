39589713d138aeffd28c7db4502f19c1f0ecec98
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2023-08-09 11:02:27 +0530

    updated image optimization test

--- tests/image-optimization.test.ts
+++ tests/image-optimization.test.ts
@@ -4,22 +4,16 @@ import sizeOf from 'image-size';
 const imagesFolderPath = './public/images/'; // Update with your image path
 const maxSize = 1 * 1024 * 1024; // 1MB in bytes
 
-test('Image optimization test', () => {
-    const oversizedImages: string[] = [];
-
+describe('Image Optimization', () => {
     fs.readdirSync(imagesFolderPath).forEach((file) => {
         const filePath = imagesFolderPath + file;
         const dimensions = sizeOf(filePath);
         const imageSize = fs.statSync(filePath).size;
 
-        if (imageSize > maxSize) {
-            oversizedImages.push(file);
-        }
-    });
+        const testTitle = `Image size test for ${file}`;
 
-    if (oversizedImages.length > 0) {
-        const oversizedFilesList = oversizedImages.join(', ');
-        console.error(`The following images are bigger than 1MB: ${oversizedFilesList}`);
-        expect(oversizedImages.length).toBe(0);
-    }
+        test(testTitle, () => {
+            expect(imageSize).toBeLessThanOrEqual(maxSize);
+        });
+    });
 });
