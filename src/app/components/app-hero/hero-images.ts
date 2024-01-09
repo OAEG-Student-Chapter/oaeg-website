568b9e598d937029433036f9ad3fa7c37d0e96a7
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2024-01-09 12:37:48 +0530

    updated images and hero section

--- src/app/components/app-hero/hero-images.ts
+++ src/app/components/app-hero/hero-images.ts
@@ -2,13 +2,18 @@ export interface IHeroImage { caption: string; url: string, title: string }
 
 export const heroImages :IHeroImage[] = [
     {
-        url: '/images/main-bg.png',
-        title: "Title 1",
-        caption: "Caption 1"
+        url: '/images/hero/what-it-takes-to-be-a-ceo-3.png',
+        title: "What It Takes To Be A CEO",
+        caption: "2022"
     },
     {
-        url: '/images/exco.jpg',
+        url: '/images/exco2023.jpg',
         title: "Board of Officials",
         caption: "2022/2023"
+    },
+    {
+        url: '/images/hero/what-it-takes-to-be-a-ceo-1.webp',
+        title: "What It Takes To Be A CEO",
+        caption: "2018"
     }
 ]
