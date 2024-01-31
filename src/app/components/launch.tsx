f5d3563510f755ca823d9e0394aada97ac5c31aa
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2024-01-31 17:48:40 +0530

    launch animation

--- src/app/components/launch.tsx
+++ src/app/components/launch.tsx
@@ -1,23 +1,151 @@
 'use client';
 import JSConfetti from "js-confetti";
-import {useEffect} from "react";
+import {ReactNode, useEffect, useState} from "react";
+import textTheme from "@/lib/fonts";
 
-const Launch = () => {
-    const colors = ["#880000", "#FFBE34", "#ffffff"];
+enum LaunchStates {
+    PreLaunch,
+    PostLaunch,
+    Active
+}
 
-    function confetti() {
+const Launch = ({app}: { app: ReactNode }) => {
+
+    const [isLaunchActive, setIsLaunchActive] = useState(false);
+    const [isLaunched, setIsLaunched] = useState(false);
+
+    const [state, setState] = useState(LaunchStates.PreLaunch);
+    // set to true to hide the launch initially
+    const [hideLaunch, setHideLaunch] = useState(true);
+
+    const _line1 = "Introducing";
+    const _line2 = "the Official Website of the";
+    const _line3 = "Old Anandian Engineers' Guild";
+
+    const _lines = [_line1, _line2, _line3];
+
+    const [lines, setLines] = useState(_lines);
+
+    // effect to initialize variables
+    useEffect(() => {
+        setIsLaunchActive(true);
+        const _isLaunched = false;
+        setIsLaunched(_isLaunched);
+        setHideLaunch(_isLaunched);
+    }, []);
+
+    // function to handle launch
+    const launch = () => {
+        setIsLaunched(true);
+        setState(LaunchStates.PostLaunch);
+
+        // after 5s setState to Active
+        setTimeout(() => {
+            setState(LaunchStates.Active);
+        }, 5000);
+    }
+
+    // set isLaunch variable in the store to true
+    const _setIsLaunched = () => {
+        if (isLaunched) {
+            //TODO: call the api
+        }
+    }
+
+    // set isLaunchActive state in component to true
+    const _setIsLaunchActive = () => {
+        if (!isLaunchActive) {
+            setIsLaunchActive(true);
+        }
+    }
+
+    // effect to throw confetti
+    useEffect(() => {
+        if (isLaunched && state === LaunchStates.Active) {
+            // throw confetti
+            //delay by 500ms
+            setTimeout(() => {
+                new Confetti().throw();
+
+            }, 500);
+            // after 1s hide the launch
+            setTimeout(() => {
+                setHideLaunch(true);
+            }, 1000);
+        }
+    }, [state]);
+
+    return <>
+    {
+        !hideLaunch && <div id={"launch"} className={`fixed z-[9999] w-full top-0 left-0  min-h-screen max-h-screen h-screen`}>
+            <Slide
+                currentState={state}
+                slideState={LaunchStates.PreLaunch}>
+                <div className={`flex flex-col justify-center items-center p-24 bg-white h-full`}>
+                    <p className={`text-xl mb-6 text-black text-center ${textTheme.body.className}`}>Good things are
+                        about
+                        to happen
+                        when
+                        you click</p>
+                    <button className={`shadow-md rounded-xl bg-theme-yellow p-4 text-3xl font-bold 
+        ${textTheme.title.className}`}
+                            onClick={() => {launch()}}
+                    >Launch 🚀
+                    </button>
+                </div>
+            </Slide>
+            <Slide currentState={state} slideState={LaunchStates.PostLaunch}>
+                <div id={"post-launch"} className={`h-full flex flex-col 
+                justify-center items-center bg-black p-24`}>
+                    <div className={`bg-black z-1 absolute top-0 left-0 w-full h-full transition-transform
+                     delay-1000 duration-1000 ${state === LaunchStates.PostLaunch ? "translate-x-full" : ""}`}></div>
+                    <p className={`text-5xl text-white mb-5 tracking-wide ${textTheme.title.className}`}>{lines[0]}</p>
+                    <p className={`text-3xl text-white mb-4 tracking-wide ${textTheme.title.className}`}>{lines[1]}</p>
+                    <p className={`text-6xl text-white tracking-wide ${textTheme.title.className}`}>{lines[2]}</p>
+                </div>
+            </Slide>
+        </div>
+    }
+        {
+            isLaunchActive && app
+        }
+    </>
+}
+
+export default Launch;
+
+const Slide = ({currentState, slideState, children}: {
+    currentState: LaunchStates,
+    slideState: LaunchStates,
+    children: ReactNode
+}) => {
+
+    const [translate, setTranslate] = useState("");
+
+    useEffect(() => {
+        let count = currentState.valueOf();
+        setTranslate(`${(100*count).toString()}vh`)
+
+    }, [currentState, slideState, children]);
+
+    return <div
+        style={{
+            transform: `translateY(-${translate})`
+        }}
+        className={`transition-transform duration-1000 h-full w-full`}>
+        {children}
+    </div>
+}
+
+class Confetti {
+    private colors = ["#880000", "#FFBE34", "#ffffff"];
+
+    public throw() {
         const jsConfetti = new JSConfetti();
         jsConfetti.addConfetti({
             confettiRadius: 5,
             confettiNumber: 500,
-            confettiColors: colors,
+            confettiColors: this.colors,
         });
     }
-
-    useEffect(() => {
-        confetti()
-    }, [])
-    return <></>
 }
-
-export default Launch;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,82 +1,80 @@
 import "../styles/globals.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import { AppFooter } from "@/app/components/app-footer/app-footer";
-import { AppHeader } from "@/app/components/app-header/app-header";
+import type {Metadata} from "next";
+import {Inter} from "next/font/google";
+import {AppFooter} from "@/app/components/app-footer/app-footer";
+import {AppHeader} from "@/app/components/app-header/app-header";
 import AppBody from "@/app/components/app-body/app-body";
 import Head from "next/head";
 import React from "react";
 import Launch from "@/app/components/launch";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
-  title: "Official Website of OAEG",
-  description: "Old Anandian Engineers Guild",
-  themeColor: "#ffffff",
-  twitter: {
-    card: "summary_large_image",
     title: "Official Website of OAEG",
-    images: [
-      {
-        url: "https://oaeg-website.pages.dev/images/logo_oaeg.png",
-      },
-    ],
-  },
-  openGraph: {
-    title: "Official Website of OAEG",
-    url: "www.oaeg.lk",
-    images: [
-      {
-        url: "https://oaeg-website.pages.dev/images/logo_oaeg.png",
-      },
-    ],
-  },
+    description: "Old Anandian Engineers Guild",
+    themeColor: "#ffffff",
+    twitter: {
+        card: "summary_large_image",
+        title: "Official Website of OAEG",
+        images: [
+            {
+                url: "https://oaeg-website.pages.dev/images/logo_oaeg.png",
+            },
+        ],
+    },
+    openGraph: {
+        title: "Official Website of OAEG",
+        url: "www.oaeg.lk",
+        images: [
+            {
+                url: "https://oaeg-website.pages.dev/images/logo_oaeg.png",
+            },
+        ],
+    },
 };
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
+                                       children,
+                                   }: {
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <Head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/icons/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/icons/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/icons/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link
-          rel="mask-icon"
-          href="/icons/safari-pinned-tab.svg"
-          color="#5bbad5"
-        />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
-      </Head>
-      <body className={inter.className}>
-      <Launch/>
-      <>
-        <AppHeader />
-        <AppBody>{children}</AppBody>
-        <AppFooter />
-      </>
-
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+        <Head>
+            <link
+                rel="apple-touch-icon"
+                sizes="180x180"
+                href="/icons/apple-touch-icon.png"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="32x32"
+                href="/icons/favicon-32x32.png"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="16x16"
+                href="/icons/favicon-16x16.png"
+            />
+            <link rel="manifest" href="/site.webmanifest"/>
+            <link
+                rel="mask-icon"
+                href="/icons/safari-pinned-tab.svg"
+                color="#5bbad5"
+            />
+            <meta name="msapplication-TileColor" content="#da532c"/>
+            <meta name="theme-color" content="#ffffff"/>
+        </Head>
+        <body className={inter.className}>
+        <Launch app={<>
+            <AppHeader/>
+            <AppBody>{children}</AppBody>
+            <AppFooter/>
+        </>}/>
+        </body>
+        </html>
+    );
 }
