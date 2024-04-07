3896ee8d5bfb3f65c41128c5a87c0fe92b6dd9ff
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2024-04-07 22:24:40 +0530

    opengraph image

---
+++ src/app/certificates/(group)/opengraph-image.tsx
@@ -0,0 +1,30 @@
+// @ts-ignore
+import { ImageResponse } from 'next/og'
+
+export const runtime = 'edge'
+
+export const alt = 'Certificate by OAEG'
+
+export const size = {
+    width: 1200,
+    height: 630,
+}
+export const contentType = 'image/png'
+
+export default async function Image({ params }: { params: { slug: string[] } }) {
+    const post = await fetch(`/${params.slug}`).then((res) =>
+        res.json()
+    )
+
+    const path = '/' + params.slug.join('/') + '.jpg'
+    const url = 'https://previews.oaeg-website.pages.dev/' + path
+
+    return new ImageResponse(
+        (
+            <img src={url} alt={''} width={1200} height={630} />
+        ),
+        {
+            ...size,
+        }
+    )
+}
