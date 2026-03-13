1435870060f872bbf8bd36faff55dcfde5a254b3
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2026-03-13 16:36:57 +0530

    chore: update with more payload files

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+import { withPayload } from '@payloadcms/next/withPayload'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -9,4 +11,4 @@ const nextConfig = {
   }
 };
 
-module.exports = nextConfig;
+export default withPayload(nextConfig)
---
+++ src/payload-config.ts
@@ -0,0 +1,28 @@
+import { postgresAdapter } from '@payloadcms/db-postgres'
+import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import path from 'path'
+import { buildConfig } from 'payload'
+import { fileURLToPath } from 'url'
+import sharp from 'sharp'
+
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+
+export default buildConfig({
+    admin: {
+        importMap: {
+            baseDir: path.resolve(dirname),
+        },
+    },
+    collections: [],
+    editor: lexicalEditor(),
+    secret: process.env.PAYLOAD_SECRET || '',
+    typescript: {
+        outputFile: path.resolve(dirname, 'payload-types.ts'),
+    },
+    db: postgresAdapter({
+        url: process.env.DATABASE_URL || '',
+    }),
+    sharp,
+    plugins: [],
+})
