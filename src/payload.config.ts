68d3c30e2817486ebbde6f747c19ccc8a12e0a64
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2026-03-13 17:25:01 +0530

    feat: initialize Payload CMS with basic configuration and add Users and Media collections.

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -4,6 +4,8 @@ import path from "path";
 import { buildConfig } from "payload";
 import { fileURLToPath } from "url";
 import sharp from "sharp";
+import { Users } from "./collections/Users";
+import { Media } from "./collections/Media";
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
@@ -14,7 +16,7 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
   },
-  collections: [],
+  collections: [Users, Media],
   editor: lexicalEditor(),
   secret: process.env.PAYLOAD_SECRET || "",
   typescript: {
