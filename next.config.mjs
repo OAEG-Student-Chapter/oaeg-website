f51a6334751a8518eeb2a77661dfc55ac5467c77
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2026-03-13 19:25:35 +0530

    Revert "chore: remove payload from next config - temporarily"
    This reverts commit bb5de0d6839e40abe7f1f52f319250b64f59928d.

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import { withPayload } from "@payloadcms/next/withPayload";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -9,4 +11,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPayload(nextConfig);
