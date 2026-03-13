bb5de0d6839e40abe7f1f52f319250b64f59928d
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2026-03-13 17:37:02 +0530

    chore: remove payload from next config - temporarily

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-import { withPayload } from "@payloadcms/next/withPayload";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -11,4 +9,4 @@ const nextConfig = {
   },
 };
 
-export default withPayload(nextConfig);
+export default nextConfig;
